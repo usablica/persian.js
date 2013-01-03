600908aee34885d09ec8f7faeded4b6efaf63177
Bersam Karbasion <bersam.k@gmail.com>
2013-01-03 15:02:47 +0330

    Now, it'll change arabic numbers to persian, too.

--- persian.js
+++ persian.js
@@ -4,11 +4,15 @@
 * return {String} Clear Persian string
 */
 String.prototype.toPersian = function () {
-    var arabicChars = ["ي", "ك", "‍", "دِ", "بِ", "زِ", "ذِ", "ِشِ", "ِسِ", "‌"],
-        persianChars = ["ی", "ک", "", "د", "ب", "ز", "ذ", "ش", "س", ""],
+    var arabicChars = ["ي", "ك", "‍", "دِ", "بِ", "زِ", "ذِ", "ِشِ", "ِسِ", "‌", "ى"],
+        persianChars = ["ی", "ک", "", "د", "ب", "ز", "ذ", "ش", "س", "", "ی"],
+        arabicNumbers = ["١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩", "٠"],
+        persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"],
         value = this.toString();
-    for (var i = 0, charsLen = arabicChars.length; i < charsLen; i++) {
-        value = value.replace(new RegExp(arabicChars[i], "g"), persianChars[i]);
+    arabic = arabicChars.concat(arabicNumbers);
+    persian = persianChars.concat(persianNumbers);
+    for (var i = 0, charsLen = arabic.length; i < charsLen; i++) {
+        value = value.replace(new RegExp(arabic[i], "g"), persian[i]);
     }
     return value;
 };
