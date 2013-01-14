71b76a1848543214f3a8da7f3fc7c049af9c1d2f
Ali Sadattalab <xbox3000@gmail.com>
2013-01-15 01:10:41 +0330

    Performance tuning, move RegExp object creation outside of replecement loop and run it once at parse time.

--- persian.js
+++ persian.js
@@ -43,13 +43,16 @@
             {fa: "۸", nf: "8"},
             {fa: "۹", nf: "9"},
             {fa: "۰", nf: "0"}
-        ];
+        ],
+        lettersRegex = _getLettersRegex();
 
     /**
      * Assign toPersian method to String Prototype to
      *  ease of String object method call without any instantiation
      */
-    String.prototype.toPersian = function() { return _toPersian(this.toString()); };
+    String.prototype.toPersian = function() {
+        return _toPersian(this.toString());
+    };
 
     //PersianJs main function/constructor, used for prototype.
     function PersianJs(str) {
@@ -59,22 +62,36 @@
     /**
      * Used for convert Arabic/English characters and numbers to Persian
      *
-     * @param {String} value 
+     * @param {String} value
      * @return {String} Returns Converted string
      * @api private
      */
     function _toPersian(value) {
 
         for (var i = 0; i < letters.length; i++) {
-            value = value.replace(new RegExp(letters[i].nf, "g"), letters[i].fa);
+            value = value.replace(lettersRegex[i], letters[i].fa);
         }
         return value;
+    }
+
+    /**
+     * Used for convert Arabic/English characters and numbers to Persian
+     *
+     * @return {Array} Returns array of letters regex object
+     * @api private
+     */
+    function _getLettersRegex () {
+        var lettersRegex = [];
+        for (var i = 0; i < letters.length; i++) {
+            lettersRegex.push(new RegExp(letters[i].nf, "g"));
+        }
+        return lettersRegex;
     }
 
     var persianJs = function(str) {
         return str ? new PersianJs(str) : str;
     }
-    
+
     //Version
     persianJs.version = VERSION;
 
