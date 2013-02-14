a31882091e3dcc3ec3a6068e51d013146c00cd49
itmard <kamalifard@datasec.ir>
2013-02-15 03:16:56 +0330

    add test.switch.js unit test for key switching

---
+++ test/test.switch.js
@@ -0,0 +1,9 @@
+//Prepare test
+require('./../persian');
+var assert = require('assert');
+describe('convert', function () {
+	it('should Convert Persian Chars to English Chars', function (done) {
+		assert.equal("qwertyuiop[]asdfghjkl;'zxcvbnm,?", persianJs("ضصثقفغعهخحجچشسیبلاتنمکگظطزرذدپو؟").switchKey());
+        done();
+    });
+});
