e2ed1157ac9e6aa5b9421cf154877baef763bbaa
Afshin Mehrabani <afshin.meh@gmail.com>
2013-03-05 10:47:46 +0330

    Change test structure

--- test/test.arabic.js
+++ test/test.arabic.js
@@ -4,11 +4,11 @@ var assert = require('assert');
 
 describe('convert', function () {
     it('should change Arabic characters to Persian', function (done) {
-        assert.equal("یکدبزذشسی", persianJs("يك‍دِبِزِذِشِسِ‌ى").toPersianChar());
+        assert.equal("یکدبزذشسی", persianJs("يك‍دِبِزِذِشِسِ‌ى").arabicChar().toString());
         done();
     });
     it('should change Arabic numbers to Persian', function (done) {
-        assert.equal("۱۲۳۴۵۶۷۸۹۰", persianJs("١٢٣٤٥٦٧٨٩٠").toPersianNumber());
+        assert.equal("۱۲۳۴۵۶۷۸۹۰", persianJs("١٢٣٤٥٦٧٨٩٠").arabicNumber().toString());
         done();
     });
 });
