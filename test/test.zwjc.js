f6dd6bbda3c2e08d3b2dd2e437bf00d7bb33519e
Reza Farhadian <R3ZA.FARHADIAN@YAHOO.COM>
2013-03-20 12:41:46 -0700

    Update test.zwjc.js

--- test/test.zwjc.js
+++ test/test.zwjc.js
@@ -3,7 +3,7 @@ require('./../persian');
 var assert = require('assert');
 describe('convert', function () {
     it('should convert to Zero-width non-joiner correction', function(done) {
-        assert.equal("می​‌‍﻿خواهم", persianJs("می خواهم").Zwjc());
+        assert.equal("می​‌‍﻿خواهمd", persianJs("می خواهم").Zwjc());
         done();
     });
 });
