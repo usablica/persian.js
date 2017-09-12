9c33b85768a184e1b6ced130539e72120b0a644a
Behnam Mohammadi (بهنام محمدی) <itten@live.com>
2017-09-12 12:28:56 +0430

    fixed bug with int number

--- persian.js
+++ persian.js
@@ -77,6 +77,7 @@
         if (!value) {
             return;
         }
+        value=value.toString();
         var arabicNumbers = ["١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩", "٠"],
             persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"];
 
@@ -99,6 +100,7 @@
         if (!value) {
             return;
         }
+        value=value.toString();
         var englishNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
             persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"];
 
